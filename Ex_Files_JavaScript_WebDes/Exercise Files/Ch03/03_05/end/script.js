46d2d107c10cb03e5e4e29bf3fe6e342062be467
Michelle Terenyi <miselke98@gmail.com>
2019-04-23 18:58:13 +0200

    Klar

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/end/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/end/script.js
@@ -6,7 +6,7 @@ document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 function estimateTotal(event) {
 	event.preventDefault();
 	
-	console.log('You submitted the form.');
+	console.log('You submitted the form');
 }
 
 })();
