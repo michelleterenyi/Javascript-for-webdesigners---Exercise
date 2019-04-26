e59f7a8474bf1cbcba34624325b290602b8990e1
Michelle Terenyi <miselke98@gmail.com>
2019-04-26 23:10:04 +0200

    klar

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
@@ -31,6 +31,13 @@
 			
 			state.focus();
 		}
+		var itemBball = document.getElementById('txt-q-bball').value,
+			itemJersey = document.getElementById('txt-q-jersey').value,
+			itemPower = document.getElementById('txt-q-power').value,
+			shippingState = state.value,
+			shippingMethod = document.querySelector('[name=r_method]:checked').value;
+
+			console.log( itemBball, itemJersey, itemPower, shippingState, shippingMethod);
 	}
 	
 	})();
